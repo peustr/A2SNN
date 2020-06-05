1382d6ef15b102ecdcce7665afdf6031e6c6c223
Panagiotis Eustratiadis <peustratiadis@gmail.com>
2020-06-05 22:25:03 +0100

    added model training checkpoints

--- metrics.py
+++ metrics.py
@@ -6,9 +6,7 @@ def accuracy(model, data_loader, device='cpu'):
     for data, target in data_loader:
         data = data.to(device)
         target = target.to(device)
-
         model.eval()
-
         logits = model(data)
         positives.append(sum(logits.argmax(-1) == target).item())
         total.append(len(data))
