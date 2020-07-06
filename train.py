1b2808420ed18d350ecb045fa288232421154898
Panagiotis Eustratiadis <peustratiadis@gmail.com>
2020-07-06 12:19:52 +0100

    storing the right variable

--- train.py
+++ train.py
@@ -71,7 +71,7 @@ def train_stochastic(model, train_loader, test_loader, args, device='cpu'):
             optimizer.step()
         train_acc.append(accuracy(model, train_loader, device=device, norm=norm_func))
         test_acc.append(accuracy(model, test_loader, device=device, norm=norm_func))
-        sigma_hist.append(model.sigma.detach().cpu().numpy())
+        sigma_hist.append(model.cov_diag.detach().cpu().numpy())
         # Checkpoint current model.
         model.save(os.path.join(args['output_path']['models'], 'ckpt'))
         # Save model with best testing performance.
