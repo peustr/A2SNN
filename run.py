e5adccaef9e4ef491d87b596d1b92f2e989bfd33
Panagiotis Eustratiadis <peustratiadis@gmail.com>
2020-07-08 13:27:53 +0100

    testing both last (ckpt) and best (ckpt_best) models

--- run.py
+++ run.py
@@ -43,43 +43,48 @@ def train(args, device):
 
 def test(args, device):
     print(args)
-    model = model_factory(args['dataset'], args['training_type'], args['feature_dim'])
-    model.to(device)
-    model.load(os.path.join(args['output_path']['models'], 'ckpt_best'))
-    model.eval()
-    if args['dataset'] == 'mnist':
-        preprocessing = None
-    elif args['dataset'] == 'cifar10':
-        preprocessing = dict(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010), axis=-3)
-    else:
-        raise NotImplementedError('Dataset not supported.')
-    fbox_model = PyTorchModel(model, bounds=(0, 1), device=device, preprocessing=preprocessing)
-    test_loader = get_data_loader(args['dataset'], args['batch_size'], False, shuffle=False, drop_last=False)
-    attack_names = ['FGSM', 'PGD', 'BIM']
-    attacks = [
-        FGSMMC(),
-        PGDMC(rel_stepsize=0.1, steps=10),
-        BIMMC(rel_stepsize=0.1),
-    ]
-    if args['dataset'] == 'mnist':
-        eps_names = ['0.0', '0.1', '0.2', '0.3', '0.4', '0.5']
-        eps_values = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]
-    elif args['dataset'] == 'cifar10':
-        eps_names = ['  0/255', '  1/255', '  2/255', '  4/255', '  8/255', ' 16/255', ' 32/255', ' 64/255', '128/255']
-        eps_values = [0. / 255, 1. / 255, 2. / 255, 4. / 255, 8. / 255, 16. / 255, 32. / 255, 64. / 255, 128. / 255]
-    print('Adversarial testing.')
-    for idx, attack in enumerate(attacks):
-        success_cum = []
-        for data, target in test_loader:
-            data = data.to(device)
-            target = target.to(device)
-            advs, _, success = attack(fbox_model, data, target, epsilons=eps_values, mc=args['monte_carlo_runs'])
-            success_cum.append(success)
-        success_cum = torch.cat(success_cum, dim=1)
-        robust_accuracy = 1 - success_cum.float().mean(axis=-1)
-        print('Attack: {}'.format(attack_names[idx]))
-        for eps_name, eps_value, accuracy in zip(eps_names, eps_values, robust_accuracy):
-            print('ε: {}, acc: {:.3f}'.format(eps_name, accuracy.item()))
+    for model_id in ('ckpt', 'ckpt_best'):
+        model = model_factory(args['dataset'], args['training_type'], args['feature_dim'])
+        model.to(device)
+        model.load(os.path.join(args['output_path']['models'], model_id))
+        model.eval()
+        if args['dataset'] == 'mnist':
+            preprocessing = None
+        elif args['dataset'] == 'cifar10':
+            preprocessing = dict(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010), axis=-3)
+        else:
+            raise NotImplementedError('Dataset not supported.')
+        fbox_model = PyTorchModel(model, bounds=(0, 1), device=device, preprocessing=preprocessing)
+        test_loader = get_data_loader(args['dataset'], args['batch_size'], False, shuffle=False, drop_last=False)
+        attack_names = ['FGSM', 'PGD', 'BIM']
+        attacks = [
+            FGSMMC(),
+            PGDMC(rel_stepsize=0.1, steps=10),
+            BIMMC(rel_stepsize=0.1),
+        ]
+        if args['dataset'] == 'mnist':
+            eps_names = ['0.0', '0.1', '0.2', '0.3', '0.4', '0.5']
+            eps_values = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]
+        elif args['dataset'] == 'cifar10':
+            eps_names = [
+                '  0/255', '  1/255', '  2/255', '  4/255', '  8/255', ' 16/255', ' 32/255', ' 64/255', '128/255']
+            eps_values = [0. / 255, 1. / 255, 2. / 255, 4. / 255, 8. / 255, 16. / 255, 32. / 255, 64. / 255, 128. / 255]
+        if model_id == 'ckpt':
+            print('Adversarial testing (LAST model).')
+        elif model_id == 'ckpt_best':
+            print('Adversarial testing (BEST model).')
+        for idx, attack in enumerate(attacks):
+            success_cum = []
+            for data, target in test_loader:
+                data = data.to(device)
+                target = target.to(device)
+                advs, _, success = attack(fbox_model, data, target, epsilons=eps_values, mc=args['monte_carlo_runs'])
+                success_cum.append(success)
+            success_cum = torch.cat(success_cum, dim=1)
+            robust_accuracy = 1 - success_cum.float().mean(axis=-1)
+            print('Attack: {}'.format(attack_names[idx]))
+            for eps_name, eps_value, accuracy in zip(eps_names, eps_values, robust_accuracy):
+                print('ε: {}, acc: {:.3f}'.format(eps_name, accuracy.item()))
     print('Finished testing.')
 
 
