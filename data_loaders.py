22cad16f69385c6047b621eae32f0847210ca509
Panagiotis Eustratiadis <peustratiadis@gmail.com>
2020-07-22 20:23:07 +0100

    to remember why i commented it out in case we introduce adv training in the future

--- data_loaders.py
+++ data_loaders.py
@@ -16,6 +16,7 @@ def get_data_loader(dataset, batch_size, train=True, shuffle=True, drop_last=Tru
                 transforms.RandomCrop(32, padding=4),
                 transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
+                # Line commented out in case we use adv. examples during training.
                 # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
             ])
         else:
