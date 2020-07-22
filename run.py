974b57f9fde3b680994518b39b277ee3b43824d9
Panagiotis Eustratiadis <peustratiadis@gmail.com>
2020-07-22 20:41:46 +0100

    after all epochs are done, train for another 10 epochs to find the best model

--- run.py
+++ run.py
@@ -46,30 +46,25 @@ def train(args, device):
 
 def test(args, device):
     print(args)
-    # for model_id in ['ckpt', 'ckpt_best']:
-    for model_id in ['ckpt_robust']:
-        model = model_factory(args['dataset'], args['training_type'], args['var_type'], args['feature_dim'])
-        model.to(device)
-        model.load(os.path.join(args['output_path']['models'], model_id))
-        model.eval()
-        test_loader = get_data_loader(args['dataset'], args['batch_size'], False, shuffle=False, drop_last=False)
-        attack_names = ['FGSM', 'PGD', 'BIM']
-        if args['dataset'] == 'mnist':
-            eps_names = ['0.0', '0.1', '0.2', '0.3', '0.4', '0.5']
-            eps_values = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]
-        elif args['dataset'] == 'cifar10':
-            eps_names = [
-                '  0/255', '  1/255', '  2/255', '  4/255', '  8/255', ' 16/255', ' 32/255', ' 64/255', '128/255']
-            eps_values = [0. / 255, 1. / 255, 2. / 255, 4. / 255, 8. / 255, 16. / 255, 32. / 255, 64. / 255, 128. / 255]
-        if model_id == 'ckpt':
-            print('Adversarial testing (LAST model).')
-        elif model_id == 'ckpt_best':
-            print('Adversarial testing (BEST model).')
-        for idx, attack in enumerate(attack_names):
-            robust_accuracy = test_attack(model, test_loader, attack, eps_values, args, device)
-            print('Attack: {}'.format(attack_names[idx]))
-            for eps_name, eps_value, accuracy in zip(eps_names, eps_values, robust_accuracy):
-                print('ε: {}, acc: {:.3f}'.format(eps_name, accuracy.item()))
+    model = model_factory(args['dataset'], args['training_type'], args['var_type'], args['feature_dim'])
+    model.to(device)
+    model.load(os.path.join(args['output_path']['models'], 'ckpt_best'))
+    model.eval()
+    test_loader = get_data_loader(args['dataset'], args['batch_size'], False, shuffle=False, drop_last=False)
+    attack_names = ['FGSM', 'PGD', 'BIM']
+    if args['dataset'] == 'mnist':
+        eps_names = ['0.0', '0.1', '0.2', '0.3', '0.4', '0.5']
+        eps_values = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]
+    elif args['dataset'] == 'cifar10':
+        eps_names = [
+            '  0/255', '  1/255', '  2/255', '  4/255', '  8/255', ' 16/255', ' 32/255', ' 64/255', '128/255']
+        eps_values = [0. / 255, 1. / 255, 2. / 255, 4. / 255, 8. / 255, 16. / 255, 32. / 255, 64. / 255, 128. / 255]
+    print('Adversarial testing.')
+    for idx, attack in enumerate(attack_names):
+        robust_accuracy = test_attack(model, test_loader, attack, eps_values, args, device)
+        print('Attack: {}'.format(attack_names[idx]))
+        for eps_name, eps_value, accuracy in zip(eps_names, eps_values, robust_accuracy):
+            print('ε: {}, acc: {:.3f}'.format(eps_name, accuracy.item()))
     print('Finished testing.')
 
 
--- train.py
+++ train.py
@@ -7,7 +7,6 @@ import torch.nn as nn
 from torch.optim import Adam
 
 from metrics import accuracy
-from test import test_attack
 from utils import normalize_cifar10
 
 
@@ -18,9 +17,10 @@ def train_vanilla(model, train_loader, test_loader, args, device='cpu'):
         norm_func = normalize_cifar10
     else:
         norm_func = None
+    epoch_margin = 10
     best_test_acc = -1.
     train_acc, test_acc = [], []
-    for epoch in range(args['num_epochs']):
+    for epoch in range(args['num_epochs'] + epoch_margin):
         for data, target in train_loader:
             data = data.to(device)
             target = target.to(device)
@@ -34,13 +34,13 @@ def train_vanilla(model, train_loader, test_loader, args, device='cpu'):
             optimizer.step()
         train_acc.append(accuracy(model, train_loader, device=device, norm=norm_func))
         test_acc.append(accuracy(model, test_loader, device=device, norm=norm_func))
-        # Checkpoint current model.
-        torch.save(model.state_dict(), os.path.join(args['output_path']['models'], 'ckpt.pt'))
-        # Save model with best testing performance.
+        print('Epoch {:03}, Train acc: {:.3f}, Test acc: {:.3f}'.format(epoch + 1, train_acc[-1], test_acc[-1]))
+        if epoch < args['num_epochs']:
+            continue
+        # Save model with best testing performance, after waiting for relative convergence.
         if test_acc[-1] > best_test_acc:
             best_test_acc = test_acc[-1]
             torch.save(model.state_dict(), os.path.join(args['output_path']['models'], 'ckpt_best.pt'))
-        print('Epoch {:03}, Train acc: {:.3f}, Test acc: {:.3f}'.format(epoch + 1, train_acc[-1], test_acc[-1]))
     # Also save the training and testing curves.
     np.save(os.path.join(args['output_path']['stats'], 'train_acc.npy'), np.array(train_acc))
     np.save(os.path.join(args['output_path']['stats'], 'test_acc.npy'), np.array(test_acc))
@@ -50,19 +50,18 @@ def train_stochastic(model, train_loader, test_loader, args, device='cpu'):
     # optimizer = Adam(model.parameters(), lr=args['lr'])
     optimizer = Adam([
         {'params': model.base.parameters()},
-        {'params': model.proto.parameters(), 'weight_decay': 0.01}
+        {'params': model.proto.parameters(), 'weight_decay': 0.1}
     ], lr=args['lr'])
     loss_func = nn.CrossEntropyLoss()
     if args['dataset'] == 'cifar10':
         norm_func = normalize_cifar10
-        eps_values = [0. / 255, 1. / 255, 2. / 255, 4. / 255, 8. / 255, 16. / 255, 32. / 255, 64. / 255, 128. / 255]
     elif args['dataset'] == 'mnist':
         norm_func = None
-        eps_values = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]
-    best_test_acc, best_test_acc_atk = -1., -1.
-    train_acc, test_acc, test_acc_atk = [], [], []
+    epoch_margin = 10
+    best_test_acc = -1.
+    train_acc, test_acc = [], []
     sigma_hist = []
-    for epoch in range(args['num_epochs']):
+    for epoch in range(args['num_epochs'] + epoch_margin):
         for data, target in train_loader:
             data = data.to(device)
             target = target.to(device)
@@ -85,21 +84,15 @@ def train_stochastic(model, train_loader, test_loader, args, device='cpu'):
             optimizer.step()
         train_acc.append(accuracy(model, train_loader, device=device, norm=norm_func))
         test_acc.append(accuracy(model, test_loader, device=device, norm=norm_func))
-        test_acc_atk.append(test_attack(model, test_loader, 'FGSM', eps_values, args, device).mean().item())
         sigma_hist.append(model.sigma.detach().cpu().numpy())
-        # Checkpoint current model.
-        model.save(os.path.join(args['output_path']['models'], 'ckpt'))
+        print('Epoch {:03}, Train acc: {:.3f}, Test acc: {:.3f}'.format(epoch + 1, train_acc[-1], test_acc[-1]))
+        if epoch < args['num_epochs']:
+            continue
         # Save model with best testing performance.
         if test_acc[-1] > best_test_acc:
             best_test_acc = test_acc[-1]
             model.save(os.path.join(args['output_path']['models'], 'ckpt_best'))
-        if test_acc_atk[-1] > best_test_acc_atk:
-            best_test_acc_atk = test_acc_atk[-1]
-            model.save(os.path.join(args['output_path']['models'], 'ckpt_robust'))
-        print('Epoch {:03}, Train acc: {:.3f}, Test acc: {:.3f}, Test adv. acc: {:.3f}'.format(
-            epoch + 1, train_acc[-1], test_acc[-1], test_acc_atk[-1]))
     # Also save the training and testing curves.
     np.save(os.path.join(args['output_path']['stats'], 'train_acc.npy'), np.array(train_acc))
     np.save(os.path.join(args['output_path']['stats'], 'test_acc.npy'), np.array(test_acc))
-    np.save(os.path.join(args['output_path']['stats'], 'test_acc_atk.npy'), np.array(test_acc_atk))
     np.save(os.path.join(args['output_path']['stats'], 'sigma_hist.npy'), np.array(sigma_hist))
