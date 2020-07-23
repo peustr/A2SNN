2d9fb264cca5c0a93fb8607f111e2a504ea78daa
Panagiotis Eustratiadis <peustratiadis@gmail.com>
2020-07-23 16:21:12 +0100

    applied projected subgradient method w = w / w.norm()

--- train.py
+++ train.py
@@ -68,9 +68,6 @@ def train_stochastic(model, train_loader, test_loader, args, device='cpu'):
             optimizer.zero_grad()
             # (w^T Sigma w) regularization.
             if args['reg_type'] == 'wSw':
-                # Force w to have unit norm (so it doesn't explode).
-                with torch.no_grad():
-                    model.proto.weight.data = model.proto.weight / model.proto.weight.norm()
                 omega = (model.proto.weight @ model.sigma @ model.proto.weight.T).diagonal().sum()
                 loss = loss_func(logits, target) + args['reg_weight'] * omega
             elif args['reg_type'] == 'max_ent':
@@ -81,6 +78,10 @@ def train_stochastic(model, train_loader, test_loader, args, device='cpu'):
                 raise NotImplementedError('Regularization "{}" not supported.'.format(args['reg_type']))
             loss.backward()
             optimizer.step()
+            # For (w^T Sigma w) regularization, force w to have unit norm (so it doesn't explode).
+            if args['reg_type'] == 'wSw':
+                with torch.no_grad():
+                    model.proto.weight.data = model.proto.weight / model.proto.weight.norm()
         train_acc.append(accuracy(model, train_loader, device=device, norm=norm_func))
         test_acc.append(accuracy(model, test_loader, device=device, norm=norm_func))
         sigma_hist.append(model.sigma.detach().cpu().numpy())
