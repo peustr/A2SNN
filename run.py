23f021746c411901f7c2e2ab2b292b6b084b9c18
Panagiotis Eustratiadis <peustratiadis@gmail.com>
2020-08-16 15:04:51 +0100

    added C&W attack in testing pipeline

--- run.py
+++ run.py
@@ -51,7 +51,7 @@ def test(args, device):
     model.load(os.path.join(args['output_path']['models'], 'ckpt_best'))
     model.eval()
     test_loader = get_data_loader(args['dataset'], args['batch_size'], False, shuffle=False, drop_last=False)
-    attack_names = ['FGSM', 'PGD', 'BIM']
+    attack_names = ['FGSM', 'PGD', 'BIM', 'C&W']
     if args['dataset'] == 'mnist':
         eps_names = ['0.0', '0.1', '0.2', '0.3', '0.4', '0.5']
         eps_values = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]
